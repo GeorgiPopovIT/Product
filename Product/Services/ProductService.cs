95006520a6978be58411f3f7da06271dfb9c7033
GeorgiPopovIT <gogopopo26062003@gmail.com>
2024-05-22 22:32:29 +0300

    Add method get all products.

--- Product/Services/ProductService.cs
+++ Product/Services/ProductService.cs
@@ -1,4 +1,5 @@
-﻿using Grpc.Core;
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
 using ProductShop.Data;
 
 namespace ProductShop.Services;
@@ -6,6 +7,21 @@ namespace ProductShop.Services;
 public class ProductService(ProductDbContext dbContext) : Product.ProductBase
 {
     private readonly ProductDbContext _dbContext = dbContext;
+
+    public override Task<ListProducts> GetProducts(Empty request, ServerCallContext context)
+    {
+        var products = this._dbContext.Products.Select(p => new ProductRequest
+        {
+            Name = p.Name,
+            Price = (double)p.Price,
+            CategoryId = p.CategoryId,
+        }).ToList();
+
+        ListProducts list = new();
+        list.Products.AddRange(products);
+
+        return Task.FromResult(list);
+    }
 
     public override Task<ProductResponse> SaveProduct(ProductRequest request, ServerCallContext context)
     {
@@ -24,4 +40,5 @@ public class ProductService(ProductDbContext dbContext) : Product.ProductBase
             IsSuccess = true
         });
     }
+
 }
